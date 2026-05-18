bc2b7e456f15d2272447251c50f2dcf7df873486
bida25-364 <bida25-364@thuto.bac.ac.bw>
2026-05-18 19:14:16 +0200

    Refactor filterMenu and remove console log/alert
    Removed console log and alert statements. Updated filterMenu function.

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-console.log("JavaScript is working!");
-alert("Welcome to Urban Eats!");
 
-function filterMenu(category) {
-  const items = document.querySelectorAll(".card");
-
-  items.forEach(item => {
-    if (category === "all") {
-      item.style.display = "block";
-    } else if (item.innerText.toLowerCase().includes(category)) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
 }
 function filterMenu(category) {
   const items = document.querySelectorAll(".card");
